338e2a71eceb5335f2d47d599f31b15460d6f481
Yin <250371746@qq.com>
2018-09-17 23:38:13 +0800

    格式化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,125 +7,126 @@
 </template>
 
 <script>
-import $ from 'jquery'
-import echarts from 'echarts'
+import $ from "jquery";
+import echarts from "echarts";
 
 let myChart = null;
-console.log(myChart)
-let _data = {}
+console.log(myChart);
+let _data = {};
 
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
       data: [],
-      index: 0,
+      index: 0
     };
   },
-  mounted(){
-    myChart = echarts.init(document.getElementById('main'))
+  mounted() {
+    myChart = echarts.init(document.getElementById("main"));
     const _this = this;
-    $.post(
-      "http://localhost:8000/initialize",
-      {},
-      function(res){
-        let resObj = JSON.parse(res);
-        _this.data = resObj.data.map(item=>JSON.parse(item))
-        _data = resObj.data.map(item=>JSON.parse(item))
-        let option = {
-            tooltip: {
-                trigger: 'item',
-                //triggerOn: 'mousemove'
+    $.post("http://localhost:8000/initialize", {}, function(res) {
+      let resObj = JSON.parse(res);
+      _this.data = resObj.data.map(item => JSON.parse(item));
+      _data = resObj.data.map(item => JSON.parse(item));
+      let option = {
+        tooltip: {
+          trigger: "item"
+          //triggerOn: 'mousemove'
+        },
+        series: [
+          {
+            type: "tree",
+            data: [_data[_this.index]],
+            left: "5%",
+            right: "5%",
+            top: "8%",
+            bottom: "20%",
+            symbol: "emptyCircle",
+            orient: "vertical",
+            expandAndCollapse: true,
+            label: {
+              normal: {
+                position: "top",
+                rotate: 0,
+                verticalAlign: "middle",
+                align: "left",
+                fontSize: 11
+              }
             },
-            series: [{
-                type: 'tree',
-                data: [_data[_this.index]],
-                left: '5%',
-                right: '5%',
-                top: '8%',
-                bottom: '20%',
-                symbol: 'emptyCircle',
-                orient: 'vertical',
-                expandAndCollapse: true,
-                label: {
-                    normal: {
-                        position: 'top',
-                        rotate: 0,
-                        verticalAlign: 'middle',
-                        align: 'left',
-                        fontSize: 11
-                    }
-                },
-                leaves: {
-                    label: {
-                        normal: {
-                            position: 'bottom',
-                            rotate: 0,
-                            verticalAlign: 'middle',
-                            align: 'center'
-                        }
-                    }
-                },
-                animationDurationUpdate: 750
-            }]
-        }
+            leaves: {
+              label: {
+                normal: {
+                  position: "bottom",
+                  rotate: 0,
+                  verticalAlign: "middle",
+                  align: "center"
+                }
+              }
+            },
+            animationDurationUpdate: 750
+          }
+        ]
+      };
 
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
-      }
-    )
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    });
   },
   methods: {
     next_page() {
       let currentIndex = this.index;
       this.index += 1;
       currentIndex++;
-        let option = {
-            tooltip: {
-                trigger: 'item',
-                //triggerOn: 'mousemove'
+      let option = {
+        tooltip: {
+          trigger: "item"
+          //triggerOn: 'mousemove'
+        },
+        series: [
+          {
+            type: "tree",
+            data: [_data[currentIndex]],
+            left: "5%",
+            right: "5%",
+            top: "8%",
+            bottom: "20%",
+            symbol: "emptyCircle",
+            orient: "vertical",
+            expandAndCollapse: true,
+            label: {
+              normal: {
+                position: "top",
+                rotate: 0,
+                verticalAlign: "middle",
+                align: "left",
+                fontSize: 11
+              }
             },
-            series: [{
-                type: 'tree',
-                data: [_data[currentIndex]],
-                left: '5%',
-                right: '5%',
-                top: '8%',
-                bottom: '20%',
-                symbol: 'emptyCircle',
-                orient: 'vertical',
-                expandAndCollapse: true,
-                label: {
-                    normal: {
-                        position: 'top',
-                        rotate: 0,
-                        verticalAlign: 'middle',
-                        align: 'left',
-                        fontSize: 11
-                    }
-                },
-                leaves: {
-                    label: {
-                        normal: {
-                            position: 'bottom',
-                            rotate: 0,
-                            verticalAlign: 'middle',
-                            align: 'center'
-                        }
-                    }
-                },
-                animationDurationUpdate: 750
-            }]
-        }
-        myChart.setOption(option);
+            leaves: {
+              label: {
+                normal: {
+                  position: "bottom",
+                  rotate: 0,
+                  verticalAlign: "middle",
+                  align: "center"
+                }
+              }
+            },
+            animationDurationUpdate: 750
+          }
+        ]
+      };
+      myChart.setOption(option);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
