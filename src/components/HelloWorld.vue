3ecc7ac44210ad8719b2b129f9d4b471a90bb798
Yin <250371746@qq.com>
2018-09-17 23:34:00 +0800

    下一页功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,124 @@
 <template>
   <div class="hello">
+    <button>上一条</button>
+    <div id="main" style="width: 1200px;height:400px;"></div>
+    <button @click="next_page">下一条</button>
   </div>
 </template>
 
 <script>
 import $ from 'jquery'
+import echarts from 'echarts'
+
+let myChart = null;
+console.log(myChart)
+let _data = {}
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      data: []
+      data: [],
+      index: 0,
     };
   },
   mounted(){
+    myChart = echarts.init(document.getElementById('main'))
     const _this = this;
     $.post(
       "http://localhost:8000/initialize",
       {},
       function(res){
         let resObj = JSON.parse(res);
-        console.log(resObj)
+        _this.data = resObj.data.map(item=>JSON.parse(item))
+        _data = resObj.data.map(item=>JSON.parse(item))
+        let option = {
+            tooltip: {
+                trigger: 'item',
+                //triggerOn: 'mousemove'
+            },
+            series: [{
+                type: 'tree',
+                data: [_data[_this.index]],
+                left: '5%',
+                right: '5%',
+                top: '8%',
+                bottom: '20%',
+                symbol: 'emptyCircle',
+                orient: 'vertical',
+                expandAndCollapse: true,
+                label: {
+                    normal: {
+                        position: 'top',
+                        rotate: 0,
+                        verticalAlign: 'middle',
+                        align: 'left',
+                        fontSize: 11
+                    }
+                },
+                leaves: {
+                    label: {
+                        normal: {
+                            position: 'bottom',
+                            rotate: 0,
+                            verticalAlign: 'middle',
+                            align: 'center'
+                        }
+                    }
+                },
+                animationDurationUpdate: 750
+            }]
+        }
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
       }
     )
+  },
+  methods: {
+    next_page() {
+      let currentIndex = this.index;
+      this.index += 1;
+      currentIndex++;
+        let option = {
+            tooltip: {
+                trigger: 'item',
+                //triggerOn: 'mousemove'
+            },
+            series: [{
+                type: 'tree',
+                data: [_data[currentIndex]],
+                left: '5%',
+                right: '5%',
+                top: '8%',
+                bottom: '20%',
+                symbol: 'emptyCircle',
+                orient: 'vertical',
+                expandAndCollapse: true,
+                label: {
+                    normal: {
+                        position: 'top',
+                        rotate: 0,
+                        verticalAlign: 'middle',
+                        align: 'left',
+                        fontSize: 11
+                    }
+                },
+                leaves: {
+                    label: {
+                        normal: {
+                            position: 'bottom',
+                            rotate: 0,
+                            verticalAlign: 'middle',
+                            align: 'center'
+                        }
+                    }
+                },
+                animationDurationUpdate: 750
+            }]
+        }
+        myChart.setOption(option);
+    }
   }
 }
 </script>
